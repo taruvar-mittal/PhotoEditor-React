543b414ad3e5799d52b870041cc51da215636191
taruvar-mittal <mittaltaruvar@gmail.com>
2021-02-04 21:44:20 +0530

    added sidebar

---
+++ photoeditor/src/SidebarItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SidebarItem() {
+    return (
+        <button className="sidebar-item">
+            Sidebar Item
+        </button>
+    )
+}
+
+export default SidebarItem;
